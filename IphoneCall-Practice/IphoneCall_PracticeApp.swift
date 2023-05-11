30808f8b543a350fc5cb328528bbfefe6ae18fe6
kine <bluekine78@gmail.com>
2023-05-11 11:54:03 +0900

    Initial Commit

---
+++ IphoneCall-Practice/IphoneCall_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneCall_PracticeApp.swift
+//  IphoneCall-Practice
+//
+//  Created by Kain on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct IphoneCall_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
